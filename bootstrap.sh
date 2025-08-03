8d96d71129fd045a59314126cc09911d0370bb50
Ram Alvapillai <ramanan.a125@gmail.com>
2025-08-03 13:47:15 -0700

    Add oh my zsh installation to the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,13 +14,22 @@ echo
 echo "Running stow on all directories except package-manager-backup..."
 
 for dir in */; do
-  # Remove trailing slash
   dir=${dir%/}
   if [[ "$dir" != "package-manager-backup" ]]; then
     echo "Stowing $dir..."
     stow "$dir"
   fi
 done
+
+echo
+echo "Installing Oh My Zsh if not already installed..."
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  # Install Oh My Zsh unattended
+  RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "Oh My Zsh is already installed."
+fi
 
 echo
 echo "Bootstrap complete!"
